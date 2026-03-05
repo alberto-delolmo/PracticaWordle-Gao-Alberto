921fe5606221909cbc020e26a279d31f072b63ed
Alberto Del Olmo Barrés <al404264@uji.es>
2026-03-05 13:17:13 +0100

    WordEvaluator.ts Refactor

--- src/Modelo/WordEvaluator.ts
+++ src/Modelo/WordEvaluator.ts
@@ -1,15 +1,11 @@
 import { LetterState } from "./LetterState.js";
 
 export class WordEvaluator {
-    
-    //Uso los _ de forma que elimino las letras que ya se han evualuado para que si
-    //hay doble coincidencia no vuelva a ocurrir --> por ejemplo: 
-    // SALDO (palabra a acertar) y LLAVE (intento)
+
     evaluate(wordTry: string, wordTarget: string): LetterState[] {
         const result: LetterState[] = new Array(wordTry.length);
         const targetLetters = wordTarget.split("");
         
-        //Miramos aquellas letras correctas
         for(let i = 0; i < wordTry.length ; i++){
             if (wordTry[i] == wordTarget[i]){
                 result[i] = LetterState.Correct;
@@ -19,18 +15,15 @@ export class WordEvaluator {
 
         
         for (let i = 0 ; i < wordTry.length; i++){
-            //Si es correcta continuamos con la siguiente
             if (result[i] == LetterState.Correct){
                 continue;
             }
             const index = targetLetters.indexOf(wordTry[i]);
             
-            //Miramos si se encuentra en otra posición y por tanto es amarilla
             if (index != -1) {
                 result[i] = LetterState.Misplaced;
                 targetLetters[index] = "_";
             }else{
-                //Si no son rojas
                 result[i] = LetterState.Wrong
             }
         }
